98a2ad4806440b995c837de73d342acf76ef64ca
roman <romanze1706@gmail.com>
2023-12-12 19:43:27 +0300

    Добавлены необходимые библиотеки

--- src/main/java/com/system/tasks/dto/TaskAuthDto.java
+++ src/main/java/com/system/tasks/dto/TaskAuthDto.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
---
+++ src/main/java/com/system/tasks/security/TaskSecurity.java
@@ -0,0 +1,12 @@
+package com.system.tasks.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class TaskSecurity {
+}
