42da2b71d0a905b538a049372c45a44cff4c7844
roman <romanze1706@gmail.com>
2023-12-12 19:23:34 +0300

    Добавлены необходимые репозитории

---
+++ src/main/java/com/system/tasks/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.system.tasks.repository;
+
+import com.system.tasks.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
